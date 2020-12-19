248db63654fa21408196bd3c6d2a5e16721a5171
Collins Koech <collinskoechck34@gmail.com>
2020-12-20 00:04:18 +0300

    update

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import LotsOfGreetings from './firstComponent/LotsOfGreetings';
 
  
 
 export default function App  (){
   return (
     <View style={styles.container}>
-      
+      <LotsOfGreetings/>
+      <Text>Hello, world!</Text>
       <StatusBar style="auto" />
     </View>
   );
