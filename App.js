4c058e2b89ea1bc6f6d0c62a7d3380d244396161
Collins Koech <collinskoechck34@gmail.com>
2020-12-19 23:53:46 +0300

    update

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import NaigationBar from 'react-native-navbar';
 
  
 
-export default function App() {
+export default function App  (){
   return (
     <View style={styles.container}>
-      <NaigationBar title={titleConfig} rightButton={rightButtonConfig}/>
+      
       <StatusBar style="auto" />
     </View>
   );
