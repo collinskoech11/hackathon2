27d154a6d4146dca90a4e7a70919bd53645c30bb
Collins Koech <collinskoechck34@gmail.com>
2020-12-20 01:01:42 +0300

    update

--- secondComponent/Nav.js
+++ secondComponent/Nav.js
@@ -13,7 +13,7 @@ export default function Nav  (){
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#000',
+      backgroundColor: '#000'
     },
   });
 
